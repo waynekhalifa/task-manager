fb33e6eb062e1b53efbf04d201148519335be901
Wani Joseph <waynekhalifa.wk@gmail.com>
2023-06-21 15:09:26 +0300

    fixes permissions table

--- src/screens/Employee/PermissionsTable.tsx
+++ src/screens/Employee/PermissionsTable.tsx
@@ -1,6 +1,9 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useState } from "react";
 import { usePermissionsQuery } from "framework/permissions/getAllPermissions";
-import { useUpdatePermissions, permissionsInput } from 'framework/permissions/updatePermissions';
+import {
+  useUpdatePermissions,
+  permissionsInput,
+} from "framework/permissions/updatePermissions";
 
 interface Props {
   isDepartmentAdmin: boolean;
@@ -21,14 +24,21 @@ interface CheckedState {
   [key: string]: { [key: string]: boolean };
 }
 
-const PermissionsTable: React.FC<Props> = ({ isDepartmentAdmin, closeModal, employeeData }): JSX.Element => {
+const PermissionsTable: React.FC<Props> = ({
+  isDepartmentAdmin,
+  closeModal,
+  employeeData,
+}): JSX.Element => {
   const { data, error, isLoading } = usePermissionsQuery({});
   const [checkedState, setCheckedState] = useState<CheckedState>({});
   const [addedPermissions, setAddedPermissions] = useState<number[]>([]);
   const [removedPermissions, setRemovedPermissions] = useState<number[]>([]);
   const { mutateAsync: createMutation } = useUpdatePermissions();
 
-  const permissions: Permission[] = useMemo(() => data?.permissions?.data?.results || [], [data]);
+  const permissions: Permission[] = useMemo(
+    () => data?.permissions?.data?.results || [],
+    [data]
+  );
   useEffect(() => {
     const initialCheckedState: CheckedState = {};
     permissions.forEach((permission: Permission) => {
@@ -56,9 +66,9 @@ const PermissionsTable: React.FC<Props> = ({ isDepartmentAdmin, closeModal, empl
 
   useEffect(() => {
     if (employeeData?.id === undefined) return;
-    handlePermissions(employeeData?.id)
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[employeeData])
+    handlePermissions(employeeData?.id);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [employeeData]);
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <></>;
@@ -67,18 +77,21 @@ const PermissionsTable: React.FC<Props> = ({ isDepartmentAdmin, closeModal, empl
       user: Number(id),
       add: addedPermissions,
       remove: removedPermissions,
-    }
+    };
     try {
       let createInput = permissionsInput(Data);
       const data = await createMutation(createInput);
-      console.log({data})
-      closeModal()
+      console.log({ data });
+      closeModal();
     } catch (err) {
       alert(err);
     }
-  }
+  };
 
-  const compareKeysToPermissions = (key: string, permission: Permission): string => {
+  const compareKeysToPermissions = (
+    key: string,
+    permission: Permission
+  ): string => {
     if (key === "can_read") {
       return `Can view ${permission.model}`;
     } else if (key === "can_write") {
@@ -88,17 +101,25 @@ const PermissionsTable: React.FC<Props> = ({ isDepartmentAdmin, closeModal, empl
     } else if (key === "can_delete") {
       return `Can delete ${permission.model}`;
     }
-    return '';
+    return "";
   };
 
   const handleCheck = (element: string, key: string, value: boolean): void => {
     // get all permissions for the element
-    const selectedPermission = permissions.find((permission: Permission) => permission.model === element);
+    const selectedPermission = permissions.find(
+      (permission: Permission) => permission.model === element
+    );
     if (!selectedPermission) return;
-    const permissionKey: string = compareKeysToPermissions(key, selectedPermission);
-    
+    const permissionKey: string = compareKeysToPermissions(
+      key,
+      selectedPermission
+    );
+
     // filter based on key and permissionKey
-    const selectedPermissionId = permissions.find((permission: Permission) => permission.model === element && permission.name === permissionKey)?.id;
+    const selectedPermissionId = permissions.find(
+      (permission: Permission) =>
+        permission.model === element && permission.name === permissionKey
+    )?.id;
     if (!selectedPermissionId) return;
 
     const isToRemove = addedPermissions.includes(selectedPermissionId); // true if permission is in addedPermissions
@@ -106,18 +127,26 @@ const PermissionsTable: React.FC<Props> = ({ isDepartmentAdmin, closeModal, empl
 
     if (isToRemove) {
       // remove from addedPermissions
-      setAddedPermissions(addedPermissions.filter((permissionId: number) => permissionId !== selectedPermissionId));
-      setRemovedPermissions([...removedPermissions, selectedPermissionId])
+      setAddedPermissions(
+        addedPermissions.filter(
+          (permissionId: number) => permissionId !== selectedPermissionId
+        )
+      );
+      setRemovedPermissions([...removedPermissions, selectedPermissionId]);
     } else if (isToAdd) {
       // remove from removedPermissions
-      setRemovedPermissions(removedPermissions.filter((permissionId: number) => permissionId !== selectedPermissionId));
-      setAddedPermissions([...addedPermissions, selectedPermissionId])
-    } else if (!isToRemove && !isToAdd && value) { 
+      setRemovedPermissions(
+        removedPermissions.filter(
+          (permissionId: number) => permissionId !== selectedPermissionId
+        )
+      );
+      setAddedPermissions([...addedPermissions, selectedPermissionId]);
+    } else if (!isToRemove && !isToAdd && value) {
       // add to addedPermissions
-      setAddedPermissions([...addedPermissions, selectedPermissionId])
+      setAddedPermissions([...addedPermissions, selectedPermissionId]);
     }
   };
-  console.log({addedPermissions, removedPermissions})
+  console.log({ addedPermissions, removedPermissions });
 
   const handleTableHeader = (): JSX.Element => {
     return (
@@ -136,7 +165,8 @@ const PermissionsTable: React.FC<Props> = ({ isDepartmentAdmin, closeModal, empl
 
   const handleTableContent = (element: string): JSX.Element[] => {
     return permissionTypes.map((permissionType, i) => {
-      const isChecked = checkedState[element]?.[permissionType.key] || isDepartmentAdmin;
+      const isChecked =
+        checkedState[element]?.[permissionType.key] || isDepartmentAdmin;
       return (
         <td key={`content-${i}`} className="text-center">
           <input
@@ -150,12 +180,16 @@ const PermissionsTable: React.FC<Props> = ({ isDepartmentAdmin, closeModal, empl
                 ...checkedState,
                 [element]: {
                   ...checkedState[element],
-                  [permissionType.key]: !checkedState[element]?.[permissionType.key],
+                  [permissionType.key]:
+                    !checkedState[element]?.[permissionType.key],
                 },
               };
               setCheckedState(newCheckedState);
-              handleCheck(element, permissionType.key, !checkedState[element]?.[permissionType.key]);
-
+              handleCheck(
+                element,
+                permissionType.key,
+                !checkedState[element]?.[permissionType.key]
+              );
             }}
           />
         </td>
@@ -164,7 +198,11 @@ const PermissionsTable: React.FC<Props> = ({ isDepartmentAdmin, closeModal, empl
   };
 
   const handleTableRow = (): JSX.Element[] => {
-    const mainCategories = [...new Set(permissions.map((permission: any) => permission.model))];
+    const mainCategories = [
+      ...Array.from(
+        new Set(permissions.map((permission: any) => permission.model))
+      ),
+    ];
     return mainCategories.map((element: string, i) => (
       <tr key={`row-${i}`}>
         <td className="fw-bold">{element}</td>
